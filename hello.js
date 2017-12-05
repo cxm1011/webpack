1b7db34ec802c6aad73f61b2de6dcbe0f8e29213
cxm <978797549@qq.com>
2017-12-05 22:54:49 +0800

    add

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 
 require('./world.js');
+//css-loader! 作用使得webpack可以处理css文件
+//style-loader! 作用将css-loader处理完的文件新建一个标签，插入到html中，当打包后的文件被引入后，css效果会执行
 require('style-loader!css-loader!./style.css');
+
 function hello(str){
 	alert(str);
 }
